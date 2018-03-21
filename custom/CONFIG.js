f9e2f7b47815cba841fe81d3e66f5257c279882f
Xuefeng Ding <xuefeng.ding@live.cn>
2018-03-21 22:20:18 +0800

    comments

--- custom/CONFIG.js
+++ custom/CONFIG.js
@@ -8,45 +8,60 @@ config['last_update'] = 0;
 config['use_favorites'] = 0;
 config['standby_after']	= 0;
 config['hide_topbar'] = 1;
+config['translate_windspeed'] = 1;
+
 
 var blocks = {}
+/*室外环境 (彩云天气插件）*/
+
+//pm2.5
 blocks[1] = {}
 blocks[1]['width'] = 4;
 blocks[1]['title'] = 'PM2.5';
 blocks[1]['switch'] = true;
 
+//风速
+blocks[3] = {}
+blocks[3]['width'] = 4;
+blocks[3]['title'] = '';
+blocks[3]['hide_data'] = true;
+//温度
 blocks[4] = {}
 blocks[4]['width'] = 4;
 blocks[4]['title'] = '温度';
 blocks[4]['switch'] = true;
 
-blocks[3] = {}
-blocks[3]['width'] = 4;
-blocks[3]['title'] = '风速';
-
+//湿度
 blocks[5] = {}
 blocks[5]['width'] = 4;
 blocks[5]['title'] = '湿度';
 
+
+/*Dummy Device*/
 blocks[8] = {}
 blocks[8]['width'] = 4;
-blocks[8]['title'] = '卧室灯';
 blocks[8]['switch'] = true;
 
+
+/*小米净化器插件*/
+//pm2.5
 blocks[9] = {}
 blocks[9]['width'] = 4;
 blocks[9]['title'] = 'PM2.5';
 blocks[9]['switch'] = true;
 
+//净化器开关
 blocks[10] = {}
 blocks[10]['width'] = 4;
 blocks[10]['title'] = '空气净化器';
 blocks[10]['switch'] = true;
 
+//湿度
 blocks[11] = {}
 blocks[11]['width'] = 4;
 blocks[11]['title'] = '湿度';
 blocks[11]['switch'] = true;
+
 
 blocks['blocktitle_1'] = {}
 blocks['blocktitle_1']['key'] = 'blocktitle_1';
@@ -85,20 +100,23 @@ screens['default'] = {}
 screens['default']['maxwidth'] = 1920;
 screens['default']['maxheight'] = 1080;
 
-screens['default'][1] = {}
-screens['default'][1]['background'] = 'bg8.jpg';
-screens['default'][1]['columns'] = [1,2,3]
-
-
+//screens['default'][1] = {}
+//screens['default'][1]['background'] = 'bg8.jpg';
+//screens['default'][1]['columns'] = [1,2,3]
 
 columns[101] = {}
-columns[101]['blocks'] = ['clock', 1,4,3,'5_2'];
+columns[101]['blocks'] = ['clock', 1,4,3,'5_2','blocktitle_2',9,'11_2',10,8];
 screens['tablet'] = {}
 screens['tablet']['maxwidth'] = 1024;
 screens['tablet']['maxheight'] = 768;
 screens['tablet'][1] = {}
 screens['tablet'][1]['background'] = 'bg9.jpg';
 screens['tablet'][1]['columns'] = [101]
+
+
+screens['default'][1] = {}
+screens['default'][1]['background'] = 'bg8.jpg';
+screens['default'][1]['columns'] = [101]
 
 //screens[1] = {}
 //screens[1]['background'] = 'bg8.jpg';
